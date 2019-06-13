385f11d3ad0cc578f835441771845e1363e10068
Esteban Hernandez <contact@mksmdesign.com>
2019-06-13 15:38:17 -0600

    Finished battle bot demo

--- app.js
+++ app.js
@@ -1,68 +1,65 @@
-//CONSOLE LOG AT START OF ALL NECESSARY FILES/COMPONENTS/FUNCTIONS TO MAKE SURE EXPECTED EVENTS ARE ACTUALLY OCCURRING
-console.log("script connected");
+let botNameInput = document.getElementById("name-input");
+let botAtkPwrInput = document.getElementById("attack-input");
+let botHealthInput = document.getElementById("hp-input");
+let buildBotButton = document.querySelector("button");
+let botDisplay = document.querySelector(".bot-display");
 
-//MAY USE FOR ARENA FUNCTIONALITY
-// const bots = [];
+let botName;
+let botAtkPower;
+let botHp;
 
-//THIS ADDS AN ONSUBMIT EVENT TO OUR HTML FORM SO THAT WE CAN FIRE THE CREATE BOT FUNCTION
-const botForm = document.getElementById("bot-form");
-botForm.addEventListener("submit", e => {
-  e.preventDefault();
-  createBot();
+
+buildBotButton.addEventListener("click", function(event){
+  event.preventDefault();
+
+  botName = botNameInput.value;
+  botAtkPower = botAtkPwrInput.value;
+  botHp = botHealthInput.value;
+
+  if(botNameInput.value !== "" && botAtkPower != null && botHp != null){
+    buildBot(botName, botAtkPower, botHp);
+  }else{
+    
+    alert(`Unable to build bot. Our builders need more information! Please double check that you have entered your information correctly.`);
+  }
+
 });
 
-function createBot(e) {
-  console.log("hit createBot function");
 
-  let name = document.getElementById("name-input").value;
-  let attack = document.getElementById("attack-input").value;
-  let hp = document.getElementById("hp-input").value;
+function buildBot(name, attackPower, healthP){
+ 
+  let newBot = document.createElement("div");
+  newBot.classList.add("bot");
+  botDisplay.appendChild(newBot);
 
-  if (name === "" || attack === "" || hp === "") {
-    alert(
-      "Unable to build bot. Our builders need more information! Please double check that you have entered your information correctly."
-    );
-  } else {
-    //THIS CREATES A DIV FOR THE BOT THAT WE WILL ADD TO THE DISPLAY SECTION
-    let div = document.createElement("div");
+  let botNameH2 = document.createElement("h2");
+  newBot.appendChild(botNameH2);
+  botNameH2.innerText = name;
 
-    //THIS ETS CLASSNAME SO STYLING APPLIES
-    div.className = "bot";
+  let attackP = document.createElement("p");
+  attackP.id = "attack";
+  newBot.appendChild(attackP);
+  attackP.innerText = `Attack: ${attackPower}`;
 
-    //THIS GENERATES THE INNER HTML OF THE DIV WE CREATED
-    div.innerHTML = `<h2> ${name}</h2>
-    <p id="attack">Attack: ${attack}</p>
-    <p id="hp">HP: ${hp}</p>
-    <button id="delete-btn">Destroy Bot</button>`;
+  let hp = document.createElement("p");
+  hp.id = "hp";
+  newBot.appendChild(hp);
+  hp.innerText = `Health: ${healthP}`;
 
-    //THIS CREATES AN ONCLICK EVENT FOR THE BUTTON ON THE DIV WE CREATED IN ORDER TO FIRE THE REMOVE BOT FUNCTION
-    div.lastChild.addEventListener("click", removeBot);
+  let destroyButton = document.createElement("button");
+  //destroyButton.classList.add("bot");
+  newBot.appendChild(destroyButton);
+  destroyButton.innerText = "DESTROY BOT";
+  destroyButton.addEventListener("click", function(event){
+    event.preventDefault();
+    destroyBot(newBot);
+  });
 
-    ///MAY USE FOR ARENA FUNCTIONALITY
-    // let newBot = {
-    //   name,
-    //   attack,
-    //   hp
-    // };
-    // bots.push(newBot);
-
-    //THIS SELECTS THE .BOT-DISPLAY SECTION AND ADDS THE DIV WE JUST CREATED TO THE .BOTS-DISPLAY SECTION
-    let botDisplay = document.querySelector(".bot-display");
-    botDisplay.appendChild(div);
-
-    // THIS CLEARS THE INPUT FIELDS OF THE BOT BUILDER FORM AFTER CREATING THE BOT
-    document.getElementById("name-input").value = "";
-    document.getElementById("attack-input").value = "";
-    document.getElementById("hp-input").value = "";
-  }
+  botNameInput.value = "";
+  botAtkPwrInput.value = null;
+  botHealthInput.value = null;
 }
 
-// THIS DELETES BOTS FROM PAGE
-function removeBot(e) {
-  console.log("hit removeBot function");
-  if (e.target.id === "delete-btn") {
-    if (confirm("Are you sure you want to obliterate your bot?")) {
-      e.target.parentElement.remove();
-    }
-  }
+function destroyBot(bot){
+  if(confirm("Are you sure you want to obliterate your bot?")) bot.remove(); 
 }
